ce949f72abe1acf0132fbd013ef667c18812e8b2
dbda-pooja <pooja.b.chaskar@gmail.com>
2023-07-23 00:18:06 +0530

    right solution of this problem is pushed

--- Top_Interview_150/merge_strings_alternatively.py
+++ Top_Interview_150/merge_strings_alternatively.py
@@ -9,16 +9,28 @@ class Solution(object):
         n = len(word1)
         m = len(word2)
         i=0
-        while  (i < n):
-             if (len(word1) == len(word2)):
+        j = 0
+        
+        if (len(word1) == len(word2)):
+            while (i<m) and (j<n):
                 merge_string = merge_string + word1[i] + word2[i]
-             i = i+1
-        # elif(len(word1)< len(word2)):
-                         
+                i = i+1
+        elif(len(word1)< len(word2)):
+            while (i<m) and (j<n):
+                merge_string = merge_string + word1[j] + word2[i]
+                i = i+1
+                j = j+1
+            merge_string = merge_string+word2[j:]
+        else:
+            while (i<m) and (j<n):
+                merge_string = merge_string + word1[j] + word2[i]
+                i = i+1
+                j = j+1
+            merge_string = merge_string+word1[i:]
         return merge_string
                   
 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.mergeAlternately("abc","pqr"))
+    print(s.mergeAlternately("abc","p"))
