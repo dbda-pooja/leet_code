009dbcc207e3897f25ec23209fba307558a9118e
dbda-pooja <pooja.b.chaskar@gmail.com>
2023-07-24 20:50:18 +0530

    sql_practise_solution pushed

---
+++ SQL/EASY/HackerRank_question_day2.sql
@@ -0,0 +1,33 @@
+/*
+problem:
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+*/
+Answer:
+SELECT name from Employee order by name;
+
+
+/*
+problem:
+Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having 
+a salary greater than $2000 per month who have been employees for less than 10  months. Sort your result by ascending employee_id.
+*/
+Answer:
+Select name from Employee  where months<10 and salary>2000;
+
+/*
+problem:
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+Equilateral: It's a triangle with  sides of equal length.
+Isosceles: It's a triangle with  sides of equal length.
+Scalene: It's a triangle with  sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
+Answer:
+Select case
+        when A+B <= C OR A+C <= B OR B+C <= A then "Not A Triangle"
+        when A = B AND B = C  then "Equilateral"
+        when A = B OR B = C OR A = C then "Isosceles"
+        else "Scalene"
+    END 
+    from TRIANGLES;
