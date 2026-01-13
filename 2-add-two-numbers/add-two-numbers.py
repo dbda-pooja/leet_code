6b535806dbaf8121f268e49c6e806f577c268fc8
Pooja Chaskar <83908968+dbda-pooja@users.noreply.github.com>
2026-01-13 21:50:09 +0530

    Time: 8 ms (53.19%) | Memory: 12.8 MB (17.46%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        def toint(node):
+            return node.val + 10 * toint(node.next) if node else 0
+        def tolist(n):
+            node = ListNode(n % 10)
+            if n > 9:
+                node.next = tolist(n / 10)
+            return node
+        return tolist(toint(l1) + toint(l2))
